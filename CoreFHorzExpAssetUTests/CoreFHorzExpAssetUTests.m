48c5ea3a9b964acb998d66ddc4971e3b878bdfa4
Robert Kirkby <robertdkirkby@gmail.com>
2025-11-10 17:22:26 +1300

    ExpAssetu with SemiExo

--- CoreFHorzExpAssetUTests/CoreFHorzExpAssetUTests.m
+++ CoreFHorzExpAssetUTests/CoreFHorzExpAssetUTests.m
@@ -90,96 +90,93 @@ output=CoreFHorzExpAssetU_CrossTests3_d1_nosemiz(n_d_withd1,n_a,n_a_big,n_z,N_j,
 
 %% Should I add a test where an experienceassetu is just an experienceasset (n_u=1, u_grid=1, pi_u=1) and see they give same result?
 
-%%
-% SEMIEXO WITH EXPASSETU HAS NOT YET BEEN IMPLEMENTED
 
 %% Worth doing a 'clear all' here, but not necessary.
 % Mainly is so you can run second half independent of first half
 
-% %% That is all the without semiz, now with semiz
-% % From here on, it is the eight with semiz
-% % From here on, use n_d_semiz and d_grid_semiz as the inputs (instead of n_d and d_grid)
-% 
-% addpath('./CoreFHorzExpAssetUTests_subcodes/')
-% addpath('./CoreFHorzExpAssetUTests_Setup/')
-% addpath('./CoreFHorzExpAssetU_ReturnFns/')
-% addpath('./CoreFHorzExpAssetUTests_subcodes/CrossTests/')
-% 
-% addpath('./CoreFHorzExpAssetUTests_subcodes/Semiz_subcodes/')
-% addpath('./CoreFHorzExpAssetU_ReturnFns/Semiz_ReturnFns/')
-% % Uses the same setup, which already had a semi-exogenous state, just that it wasn't used.
-% CoreFHorzExpAssetU_setup
-% 
-% % For models without d1, use:
-% % n_d2_semiz and d2_grid_semiz (as n_d and d_grid)
-% % For models with d1, use:
-% % n_d_semiz and d_grid_semiz (as n_d and d_grid)
-% 
-% %% without d1, without z, without e, with semiz
-% figure_c=9;
-% output=CoreFHorzExpAssetU_nod1_noz_noe_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% % looks good
-% 
-% %% with d1, without z, without e, with semiz
-% figure_c=10;
-% output=CoreFHorzExpAssetU_d1_noz_noe_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% without d1, with z, without e, with semiz
-% figure_c=11;
-% output=CoreFHorzExpAssetU_nod1_z_noe_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% with d1, with z, without e, with semiz
-% figure_c=12;
-% output=CoreFHorzExpAssetU_d1_z_noe_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% without d1, without z, with e, with semiz
-% figure_c=13;
-% output=CoreFHorzExpAssetU_nod1_noz_e_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% with d1, without z, with e, with semiz
-% figure_c=14;
-% output=CoreFHorzExpAssetU_d1_noz_e_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% without d1, with z, with e, with semiz
-% figure_c=15;
-% output=CoreFHorzExpAssetU_nod1_z_e_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% with d1, with z, with e, with semiz
-% figure_c=16;
-% output=CoreFHorzExpAssetU_d1_z_e_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
-% 
-% % good until runs out of memory
-% 
-% %% Now some cross-tests, things like setting up a markov that is actually just an iid, make sure we get same result as just doing iid
-% output=CoreFHorzExpAssetU_CrossTests_nod1_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
-% 
-% output=CoreFHorzExpAssetU_CrossTests_d1_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
-% % all looking good :)
-% 
-% %% Now some further cross-tests, using a semi-exo that is really just a markov
-% output=CoreFHorzExpAssetU_CrossTests2_nod1_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
-% 
-% output=CoreFHorzExpAssetU_CrossTests2_d1_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
-% % all looking good :)
-% 
-% %% Do a test with a 'fake experience asset' and compare to a standard endogneous asset
-% output=CoreFHorzExpAssetU_CrossTests3_nod1_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
-% 
-% output=CoreFHorzExpAssetU_CrossTests3_d1_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
-% % all looking good :)
+%% That is all the without semiz, now with semiz
+% From here on, it is the eight with semiz
+% From here on, use n_d_semiz and d_grid_semiz as the inputs (instead of n_d and d_grid)
 
+addpath('./CoreFHorzExpAssetUTests_subcodes/')
+addpath('./CoreFHorzExpAssetUTests_Setup/')
+addpath('./CoreFHorzExpAssetU_ReturnFns/')
+addpath('./CoreFHorzExpAssetUTests_subcodes/CrossTests/')
+
+addpath('./CoreFHorzExpAssetUTests_subcodes/Semiz_subcodes/')
+addpath('./CoreFHorzExpAssetU_ReturnFns/Semiz_ReturnFns/')
+% Uses the same setup, which already had a semi-exogenous state, just that it wasn't used.
+CoreFHorzExpAssetU_setup
+
+% For models without d1, use:
+% n_d2_semiz and d2_grid_semiz (as n_d and d_grid)
+% For models with d1, use:
+% n_d_semiz and d_grid_semiz (as n_d and d_grid)
+
+%% without d1, without z, without e, with semiz
+figure_c=9;
+output=CoreFHorzExpAssetU_nod1_noz_noe_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+% looks good
+
+%% with d1, without z, without e, with semiz
+figure_c=10;
+output=CoreFHorzExpAssetU_d1_noz_noe_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% without d1, with z, without e, with semiz
+figure_c=11;
+output=CoreFHorzExpAssetU_nod1_z_noe_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% with d1, with z, without e, with semiz
+figure_c=12;
+output=CoreFHorzExpAssetU_d1_z_noe_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% without d1, without z, with e, with semiz
+figure_c=13;
+output=CoreFHorzExpAssetU_nod1_noz_e_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% with d1, without z, with e, with semiz
+figure_c=14;
+output=CoreFHorzExpAssetU_d1_noz_e_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% without d1, with z, with e, with semiz
+figure_c=15;
+output=CoreFHorzExpAssetU_nod1_z_e_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% with d1, with z, with e, with semiz
+figure_c=16;
+output=CoreFHorzExpAssetU_d1_z_e_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline,figure_c);
+
+% good until runs out of memory
+
+%% Now some cross-tests, things like setting up a markov that is actually just an iid, make sure we get same result as just doing iid
+output=CoreFHorzExpAssetU_CrossTests_nod1_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
+
+output=CoreFHorzExpAssetU_CrossTests_d1_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
+% all looking good :)
+
+%% Now some further cross-tests, using a semi-exo that is really just a markov
+output=CoreFHorzExpAssetU_CrossTests2_nod1_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
+
+output=CoreFHorzExpAssetU_CrossTests2_d1_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
+% all looking good :)
+
+%% Do a test with a 'fake experience asset' and compare to a standard endogneous asset
+output=CoreFHorzExpAssetU_CrossTests3_nod1_semiz(n_d_withoutd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withoutd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
+
+output=CoreFHorzExpAssetU_CrossTests3_d1_semiz(n_d_withd1semiz,n_a,n_a_big,n_z,N_j,d_grid_withd1semiz,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline);
+% all looking good :)
 
 
 %% THINGS NOT CHECKED
---
+++ CoreFHorzExpAssetUTests/CoreFHorzExpAssetUTests_subcodes/CrossTests/CoreFHorzExpAssetU_CrossTests2_d1_semiz.m
@@ -0,0 +1,195 @@
+function output=CoreFHorzExpAssetU_CrossTests2_d1_semiz(n_d,n_a,n_a_big,n_z,N_j,d_grid,a_grid,a_grid_big,z_grid,pi_z,Params,DiscountFactorParamNames,AgeWeightParamNames,vfoptionsbaseline,simoptionsbaseline)
+
+% For models without semiz we still need
+n_d_withoutsemiz=n_d(1:2); % d3 is semi-exo
+d_grid_withoutsemiz=d_grid(1:sum(n_d(1:2)));
+
+Params.uempbenefit=0; % Need this to make return fns the same
+Params.searcheffortcost=0; % No actual need to set this to zero, as d2 does
+% nothing in these CrossTests2 examples anyway, and d2=0 is one of the
+% choices you can make in which case the return fns are the same.
+
+% WARNING: THE z and semiz IN HERE ARE PRETTY HARDCODED!
+n_z=2;
+z_grid=[0.6;1.4];
+pi_z=[1-Params.probfindjob, Params.probfindjob;...
+    Params.problosejob, 1-Params.problosejob];
+Params.z1=z_grid(1);
+Params.z2=z_grid(2);
+
+% First, do a test in which the semiz is just a duplicate of z
+SemiExoStateFn_JustAMarkov=@(n,nprime,dsemiz,probfindjob,problosejob,z1,z2) CoreFHorzExpAssetSetup_SemiExoStateFn_JustAMarkov(n,nprime,dsemiz,probfindjob,problosejob,z1,z2);
+vfoptions.n_semiz=n_z;
+vfoptions.semiz_grid=z_grid;
+vfoptions.SemiExoStateFn=SemiExoStateFn_JustAMarkov;
+simoptions.n_semiz=n_z;
+simoptions.semiz_grid=z_grid;
+simoptions.SemiExoStateFn=SemiExoStateFn_JustAMarkov;
+simoptions.d_grid=d_grid;
+% For convenience
+n_semiz=vfoptionsbaseline.n_semiz;
+
+vfoptions.divideandconquer=1;
+% Experience asset u
+vfoptions.experienceassetu=1;
+simoptions.experienceassetu=1;
+vfoptions.aprimeFn=vfoptionsbaseline.aprimeFn;
+simoptions.aprimeFn=vfoptions.aprimeFn;
+simoptions.d_grid=d_grid;
+simoptions.a_grid=a_grid;
+vfoptions.n_u=vfoptionsbaseline.n_u;
+vfoptions.u_grid=vfoptionsbaseline.u_grid;
+vfoptions.pi_u=vfoptionsbaseline.pi_u;
+simoptions.n_u=vfoptions.n_u;
+simoptions.u_grid=vfoptions.u_grid;
+simoptions.pi_u=vfoptions.pi_u;
+
+% Setup vfoptions and simoptions
+vfoptions.n_e=vfoptionsbaseline.n_e;
+vfoptions.e_grid=vfoptionsbaseline.e_grid;
+vfoptions.pi_e=vfoptionsbaseline.pi_e;
+simoptions.n_e=simoptionsbaseline.n_e;
+simoptions.e_grid=simoptionsbaseline.e_grid;
+simoptions.pi_e=simoptionsbaseline.pi_e;
+
+ReturnFn_semiz=@(d1,d2,d3,a1prime,a1,a2,semiz,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension,uempbenefit,searcheffortcost)...
+    ReturnFn_d1_noz_noe_semiz(d1,d2,d3,a1prime,a1,a2,semiz,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension,uempbenefit,searcheffortcost);
+ReturnFn_semize=@(d1,d2,d3,a1prime,a1,a2,semiz,e,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension,uempbenefit,searcheffortcost)...
+    ReturnFn_d1_noz_e_semiz(d1,d2,d3,a1prime,a1,a2,semiz,e,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension,uempbenefit,searcheffortcost);
+
+ReturnFn_z=@(d1,d2,a1prime,a1,a2,z,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension)...
+    ReturnFn_d1_z_noe_nosemiz(d1,d2,a1prime,a1,a2,z,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension);
+ReturnFn_ze=@(d1,d2,a1prime,a1,a2,z,e,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension)...
+    ReturnFn_d1_z_e_nosemiz(d1,d2,a1prime,a1,a2,z,e,r,w,kappa_j,sigma,varphi,eta,agej,Jr,pension);
+
+
+%% Solving for model with one markov
+jequaloneDist1=zeros([n_a,n_semiz],'gpuArray');
+jequaloneDist1(1,1,ceil(n_semiz/2))=1; % no assets
+
+vfoptions1A.divideandconquer=vfoptions.divideandconquer;
+vfoptions1A.experienceassetu=vfoptions.experienceassetu;
+vfoptions1A.n_u=vfoptions.n_u;
+vfoptions1A.u_grid=vfoptions.u_grid;
+vfoptions1A.pi_u=vfoptions.pi_u;
+simoptions1A.experienceassetu=simoptions.experienceassetu;
+simoptions1A.n_u=simoptions.n_u;
+simoptions1A.u_grid=simoptions.u_grid;
+simoptions1A.pi_u=simoptions.pi_u;
+vfoptions1A.aprimeFn=vfoptions.aprimeFn;
+simoptions1A.aprimeFn=simoptions.aprimeFn;
+simoptions1A.d_grid=simoptions.d_grid;
+simoptions1A.a_grid=simoptions.a_grid;
+% First, just use z (without semiz)
+[V1A,Policy1A]=ValueFnIter_Case1_FHorz(n_d_withoutsemiz,n_a,n_z,N_j,d_grid_withoutsemiz,a_grid,z_grid,pi_z,ReturnFn_z,Params,DiscountFactorParamNames,[],vfoptions1A);
+StationaryDist1A=StationaryDist_FHorz_Case1(jequaloneDist1,AgeWeightParamNames,Policy1A,n_d_withoutsemiz,n_a,n_z,N_j,pi_z,Params,simoptions1A);
+
+vfoptions1B.divideandconquer=vfoptions.divideandconquer;
+vfoptions1B.experienceassetu=vfoptions.experienceassetu;
+vfoptions1B.n_u=vfoptions.n_u;
+vfoptions1B.u_grid=vfoptions.u_grid;
+vfoptions1B.pi_u=vfoptions.pi_u;
+simoptions1B.experienceassetu=simoptions.experienceassetu;
+simoptions1B.n_u=simoptions.n_u;
+simoptions1B.u_grid=simoptions.u_grid;
+simoptions1B.pi_u=simoptions.pi_u;
+vfoptions1B.aprimeFn=vfoptions.aprimeFn;
+simoptions1B.aprimeFn=simoptions.aprimeFn;
+simoptions1B.d_grid=simoptions.d_grid;
+simoptions1B.a_grid=simoptions.a_grid;
+% Second, use semiz (without z)
+vfoptions1B.n_semiz=vfoptions.n_semiz;
+vfoptions1B.semiz_grid=vfoptions.semiz_grid;
+vfoptions1B.SemiExoStateFn=vfoptions.SemiExoStateFn;
+simoptions1B.n_semiz=simoptions.n_semiz;
+simoptions1B.semiz_grid=simoptions.semiz_grid;
+simoptions1B.SemiExoStateFn=simoptions.SemiExoStateFn;
+simoptions1B.d_grid=simoptions.d_grid;
+[V1B,Policy1B]=ValueFnIter_Case1_FHorz(n_d,n_a,0,N_j,d_grid,a_grid,[],[],ReturnFn_semiz,Params,DiscountFactorParamNames,[],vfoptions1B);
+StationaryDist1B=StationaryDist_FHorz_Case1(jequaloneDist1,AgeWeightParamNames,Policy1B,n_d,n_a,0,N_j,[],Params,simoptions1B);
+
+Policy1Bshort=[Policy1B(1:2,:,:,:); Policy1B(4,:,:,:)]; % remove the d2 policy  (as it is not relevant, and is not in Policy1A)
+
+% size(Policy1A)
+% size(Policy1B)
+
+fprintf('Cross test 2: semiz as z, this should be zero: %2.8f \n',max(abs(V1A(:)-V1B(:))))
+fprintf('Cross test 2: semiz as z, this should be zero: %2.8f \n',max(abs(Policy1A(:)-Policy1Bshort(:))))
+fprintf('Cross test 2: semiz as z, this should be zero: %2.8f \n',max(abs(StationaryDist1A(:)-StationaryDist1B(:))))
+
+% squeeze(abs(sum(StationaryDist1A,1)-sum(StationaryDist1B,1))) % Directly check shocks without Policy.
+
+%% Solving for model with one markov and one e
+jequaloneDist1=zeros([n_a,n_semiz,vfoptions.n_e],'gpuArray');
+jequaloneDist1(1,1,ceil(n_semiz/2),ceil(vfoptions.n_e/2))=1; % no assets
+
+vfoptions2A.divideandconquer=vfoptions.divideandconquer;
+vfoptions2A.experienceassetu=vfoptions.experienceassetu;
+vfoptions2A.n_u=vfoptions.n_u;
+vfoptions2A.u_grid=vfoptions.u_grid;
+vfoptions2A.pi_u=vfoptions.pi_u;
+simoptions2A.experienceassetu=simoptions.experienceassetu;
+simoptions2A.n_u=simoptions.n_u;
+simoptions2A.u_grid=simoptions.u_grid;
+simoptions2A.pi_u=simoptions.pi_u;
+vfoptions2A.aprimeFn=vfoptions.aprimeFn;
+simoptions2A.aprimeFn=simoptions.aprimeFn;
+simoptions2A.d_grid=simoptions.d_grid;
+simoptions2A.a_grid=simoptions.a_grid;
+% First, just use z and e (without semiz)
+vfoptions2A.n_e=vfoptions.n_e;
+vfoptions2A.e_grid=vfoptions.e_grid;
+vfoptions2A.pi_e=vfoptions.pi_e;
+simoptions2A.n_e=simoptions.n_e;
+simoptions2A.e_grid=simoptions.e_grid;
+simoptions2A.pi_e=simoptions.pi_e;
+[V2A,Policy2A]=ValueFnIter_Case1_FHorz(n_d_withoutsemiz,n_a,n_z,N_j,d_grid_withoutsemiz,a_grid,z_grid,pi_z,ReturnFn_ze,Params,DiscountFactorParamNames,[],vfoptions2A);
+StationaryDist2A=StationaryDist_FHorz_Case1(jequaloneDist1,AgeWeightParamNames,Policy2A,n_d_withoutsemiz,n_a,n_z,N_j,pi_z,Params,simoptions2A);
+
+vfoptions2B.divideandconquer=vfoptions.divideandconquer;
+vfoptions2B.experienceassetu=vfoptions.experienceassetu;
+vfoptions2B.n_u=vfoptions.n_u;
+vfoptions2B.u_grid=vfoptions.u_grid;
+vfoptions2B.pi_u=vfoptions.pi_u;
+simoptions2B.experienceassetu=simoptions.experienceassetu;
+simoptions2B.n_u=simoptions.n_u;
+simoptions2B.u_grid=simoptions.u_grid;
+simoptions2B.pi_u=simoptions.pi_u;
+vfoptions2B.aprimeFn=vfoptions.aprimeFn;
+simoptions2B.aprimeFn=simoptions.aprimeFn;
+simoptions2B.d_grid=simoptions.d_grid;
+simoptions2B.a_grid=simoptions.a_grid;
+% Second, use semiz and e (without z)
+vfoptions2B.n_semiz=vfoptions.n_semiz;
+vfoptions2B.semiz_grid=vfoptions.semiz_grid;
+vfoptions2B.SemiExoStateFn=vfoptions.SemiExoStateFn;
+simoptions2B.n_semiz=simoptions.n_semiz;
+simoptions2B.semiz_grid=simoptions.semiz_grid;
+simoptions2B.SemiExoStateFn=simoptions.SemiExoStateFn;
+simoptions2B.d_grid=simoptions.d_grid;
+vfoptions2B.n_e=vfoptions.n_e;
+vfoptions2B.e_grid=vfoptions.e_grid;
+vfoptions2B.pi_e=vfoptions.pi_e;
+simoptions2B.n_e=simoptions.n_e;
+simoptions2B.e_grid=simoptions.e_grid;
+simoptions2B.pi_e=simoptions.pi_e;
+[V2B,Policy2B]=ValueFnIter_Case1_FHorz(n_d,n_a,0,N_j,d_grid,a_grid,[],[],ReturnFn_semize,Params,DiscountFactorParamNames,[],vfoptions2B);
+StationaryDist2B=StationaryDist_FHorz_Case1(jequaloneDist1,AgeWeightParamNames,Policy2B,n_d,n_a,0,N_j,[],Params,simoptions2B);
+
+Policy2Bshort=[Policy2B(1:2,:,:,:,:); Policy2B(4,:,:,:,:)]; % remove the d2 policy  (as it is not relevant, and is not in Policy2A)
+
+% size(Policy2A)
+% size(Policy2B)
+
+fprintf('Cross test 2: semiz as z (with e), this should be zero: %2.8f \n',max(abs(V2A(:)-V2B(:))))
+fprintf('Cross test 2: semiz as z (with e), this should be zero: %2.8f \n',max(abs(Policy2A(:)-Policy2Bshort(:))))
+fprintf('Cross test 2: semiz as z (with e), this should be zero: %2.8f \n',max(abs(StationaryDist2A(:)-StationaryDist2B(:))))
+
+% squeeze(abs(sum(StationaryDist2A(:,1,:,:),1)-sum(StationaryDist2B(:,1,:,:),1))) % Directly check shocks without Policy.
+% squeeze(abs(sum(StationaryDist2A(:,2,:,:),1)-sum(StationaryDist2B(:,2,:,:),1))) % Directly check shocks without Policy.
+
+
+%%
+output=struct(); % Not currently used for anything. Maybe will do so later.
+
+end
